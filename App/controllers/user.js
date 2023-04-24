2e0c9637e8125257553af43ec638c552665a5bc9
Enzo <e.valfort@epastelsoft.com>
2023-04-24 17:21:47 +0200

    add jwttoken and optimize signup w/ spread operator

--- App/controllers/user.js
+++ App/controllers/user.js
@@ -1,26 +1,54 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 exports.signup = async (req, res) => {
 	try {
-		const { firstName, lastName, email, password } = req.body;
+		const { password, ...userData } = req.body;
 
 		// Create user in database
-		const hashedPassword = await bcrypt.hash(password, 10);
 		const user = await User.create({
-			firstName,
-			lastName,
-			email,
-			password: hashedPassword,
+			...req.body,
+			password: await bcrypt.hash(req.body.password, 10),
 		});
 
-		res.json(user);
+		res.status(201).json(user);
 	} catch (error) {
 		console.error(error);
-		res.status(500).send('Error creating user');
+		res.status(500).send('Error create user');
 	}
 };
 
-exports.login = (req, res) => {
-	res.send('You are login');
+exports.login = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.findOne({ where: { email } });
+
+		if (!user) {
+			return res.status(401).json({ error: 'User Not found' });
+		}
+
+		const passwordVerify = await bcrypt.compare(password, user.password);
+
+		if (!passwordVerify) {
+			return res.status(401).json({ error: 'User sNot Found' });
+		}
+
+		const token = jwt.sign({ userId: user.id }, 'secret_key', {
+			expiresIn: '24h',
+		});
+
+		res.json({
+			user: {
+				id: user.id,
+				username: user.username,
+				email: user.email,
+			},
+			token,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error Authenticatings User');
+	}
 };
