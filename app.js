d114f7b192133cf9f9054aea01c9b75ea9d1c88a
Enzo <e.valfort@epastelsoft.com>
2023-04-28 15:19:07 +0200

    .

--- app.js
+++ app.js
@@ -10,15 +10,12 @@ db.sequelize
 	.authenticate()
 	.then(() => console.log('Database connected ...'))
 	.catch((err) => console.log(err));
-//Ajout de swagger
+
 const swaggerDocument = require('./swagger.json');
-// Définir le chemin du fichier de logs
 const logFilePath = './logs.log';
 
-// Ouvrir le fichier en mode append
 const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
 
-// Configurer le middleware de journalisation
 app.use(morgan('combined', { stream: logStream }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
